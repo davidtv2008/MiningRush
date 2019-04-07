cf1ba82837c681e11ad142d9bb794ee219265f67
david <davidtv2008@hotmail.com>
2019-04-06 17:40:08 -0700

    main menu screen

---
+++ Block.py
@@ -0,0 +1,19 @@
+import arcade
+
+
+class Block(arcade.Sprite):
+    def __init__(self, sprite_file, size, scale, row, col, block_type):
+        super().__init__(sprite_file)
+        self.append_texture(arcade.load_texture("graphics/Tiles/air.png", 0, 0, 0, 0, False, False, scale))
+        self.width = size
+        self.height = size
+        self.row = row
+        self.col = col
+        self.block_type = block_type
+
+    def __str__(self):
+        return str(self.row) + ", " + str(self.col) + ": " + self.block_type
+
+    def destroy_block(self):
+        self.block_type = "air"
+        self.set_texture(1)
